b45b2f906f5f9c38ae9028838899f105f8bd5c6f
BrijeshSukhadiya <67324310+BrijeshSukhadiya@users.noreply.github.com>
2025-04-29 21:33:31 +0200

    auth

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuestionViewSet, AnswerViewSet, VoteView
+from .views import QuestionViewSet, AnswerViewSet, VoteView, RegisterView, UserProfileView
 
 router = DefaultRouter()
 router.register(r'questions', QuestionViewSet)
@@ -8,5 +8,12 @@ router.register(r'answers', AnswerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('vote/', VoteView.as_view(), name='vote')
+    path('vote/', VoteView.as_view(), name='vote'),
+
+    # User Management
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+
+    # OAuth2
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
